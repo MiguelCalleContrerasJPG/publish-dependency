f220e100994f9f1a1dd7f93f5200eef9382cd64e
MiguelCalleContrerasJPG <mcallecontreras@gmail.com>
2025-03-05 11:42:05 -0500

    first version

---
+++ src/main/java/com/leviathan/Math.java
@@ -0,0 +1,20 @@
+package com.leviathan;
+
+public class Math {
+
+    public static int add(int n1, int n2){
+        return n1 + n2;
+    }
+
+    public static int subtract(int n1, int n2){
+        return n1 - n2;
+    }
+
+    public static int multiply(int n1, int n2){
+        return n1 * n2;
+    }
+
+    public static int divide(int n1, int n2){
+        return n1 / n2;
+    }
+}
